85d87ffdf270e933b9931f7dfccb3a8b881bf03f
Bùi Văn Kiên <kbui0212@gmail.com>
2023-10-12 02:23:47 +0700

    Modify: learn_reactjs_02 all the function and the context

--- Front End/ReactJS/learn_reactjs_02/src/App.js
+++ Front End/ReactJS/learn_reactjs_02/src/App.js
@@ -5,153 +5,107 @@ import Footer from './Footer';
 import Home from './Home';
 import NewPost from './NewPost';
 import PostPage from './PostPage';
-//import EditPost from './EditPost';
+import EditPost from './EditPost';
 import About from './About';
 import Missing from './Missing';
-import { Routes, Route, Switch, useHistory } from 'react-router-dom';
+import { Routes, Route, Switch } from 'react-router-dom';
+/*
 import { useState, useEffect } from 'react';
 import { format } from 'date-fns';
 import api from './api/posts'
-import EditPost from './EditPost';
 import useWindowSize from './hooks/useWindowSize';
 import useAxiosFetch from './hooks/useAxiosFetch';
+*/
 import { DataProvider } from './context/DataContext';
 
 function App() {
-  const [posts, setPosts] = useState([])
-  const [search, setSearch] = useState('')
-  // results is an array
-  const [searchResults, setSearchResults] = useState([])
-
-  const [postTitle, setPostTitle] = useState('');
-  const [postBody, setPostBody] = useState('');
-
-  const [editTitle, setEditTitle] = useState('');
-  const [editBody, setEditBody] = useState('');
-
-  // history
-  // const history = useHistory()
-
-  const { width } = useWindowSize()
-
-  const { data, fetchError, isLoading } = useAxiosFetch('http://localhost:3500/posts')
-
-  useEffect(() => {
-    setPosts(data)
-  }, [data])
-
   /*
-  // show data
-  useEffect(() => {
-    const fetchPosts = async () => {
+    const [posts, setPosts] = useState([])
+    const [search, setSearch] = useState('')
+    // results is an array
+    const [searchResults, setSearchResults] = useState([])
+  
+    const [postTitle, setPostTitle] = useState('');
+    const [postBody, setPostBody] = useState('');
+  
+    const [editTitle, setEditTitle] = useState('');
+    const [editBody, setEditBody] = useState('');
+  
+    // history
+    // const history = useHistory()
+  
+    const { width } = useWindowSize()
+  
+    const { data, fetchError, isLoading } = useAxiosFetch('http://localhost:3500/posts')
+  
+    useEffect(() => {
+      setPosts(data)
+    }, [data])
+  
+    useEffect(() => {
+      // search
+      const filteredResults = posts.filter(post =>
+        (post.body).toLowerCase().includes(search.toLowerCase())
+        || (post.title).toLowerCase().includes(search.toLowerCase()))
+  
+      setSearchResults(filteredResults.reverse())
+    }, [posts, search])
+  
+    const handleSubmit = async (e) => {
+      e.preventDefault();
+      const id = posts.length ? posts[posts.length - 1].id + 1 : 1;
+      const datetime = format(new Date(), 'MMMM dd, yyyy pp'); // pp for time
+      const newPost = { id, title: postTitle, datetime, body: postBody }
       try {
-        const response = await api.get('/posts')
-        setPosts(response.data)
+        const response = await api.post('/posts', response.data)
+        const allPosts = [...posts, newPost] // an array
+        setPosts(allPosts)
+        setPostTitle('')
+        setPostBody('')
+        //history.push('/')
       } catch (error) {
-        if (error.response) {
-          // not in the 200 response range 
-          console.log(error.response.data)
-          console.log(error.response.message)
-          console.log(error.response.status)
-          console.log(error.response.headers)
-        } else {
-          console.log(`Error: ${error.message}`)
-        }
+        console.log(`Error: ${error.message}`)
       }
     }
-
-    // fetch 
-    fetchPosts()
-  }, [])
-*/
-
-  useEffect(() => {
-    // search
-    const filteredResults = posts.filter(post =>
-      (post.body).toLowerCase().includes(search.toLowerCase())
-      || (post.title).toLowerCase().includes(search.toLowerCase()))
-
-    setSearchResults(filteredResults.reverse())
-  }, [posts, search])
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    const id = posts.length ? posts[posts.length - 1].id + 1 : 1;
-    const datetime = format(new Date(), 'MMMM dd, yyyy pp'); // pp for time
-    const newPost = { id, title: postTitle, datetime, body: postBody }
-    try {
-      const response = await api.post('/posts', response.data)
-      const allPosts = [...posts, newPost] // an array
-      setPosts(allPosts)
-      setPostTitle('')
-      setPostBody('')
-      //history.push('/')
-    } catch (error) {
-      console.log(`Error: ${error.message}`)
+  
+    const handleEdit = async (id) => {
+      const datetime = format(new Date(), 'MMMM dd, yyyy pp'); // pp for time
+      const updatePost = { id, title: editTitle, datetime, body: editBody }
+      try {
+        const response = await api.put(`/posts/${id}`, updatePost)
+        setPosts(posts.map(post => post.id === id ? { ...response.data } : post))
+        setEditTitle('')
+        setEditBody('')
+        //history.push('')
+      } catch (error) {
+        console.log(`Error: ${error.message}`)
+      }
     }
-  }
-
-  const handleEdit = async (id) => {
-    const datetime = format(new Date(), 'MMMM dd, yyyy pp'); // pp for time
-    const updatePost = { id, title: editTitle, datetime, body: editBody }
-    try {
-      const response = await api.put(`/posts/${id}`, updatePost)
-      setPosts(posts.map(post => post.id === id ? { ...response.data } : post))
-      setEditTitle('')
-      setEditBody('')
-      //history.push('')
-    } catch (error) {
-      console.log(`Error: ${error.message}`)
+  
+    const handleDelete = async (id) => {
+      try {
+        //await api.delete(`/posts/${id}`)
+        // const postList = posts.filter(post => post.id !== id)
+        // setPosts(postList)
+        // history.push('/') 
+      } catch (error) {
+        console.log(`Error: ${error.message}`)
+      }
     }
-  }
-
-  const handleDelete = async (id) => {
-    try {
-      //await api.delete(`/posts/${id}`)
-      // const postList = posts.filter(post => post.id !== id)
-      // setPosts(postList)
-      // history.push('/') 
-    } catch (error) {
-      console.log(`Error: ${error.message}`)
-    }
-  }
+  */
 
   return (
     <div>
       <DataProvider>
-        <Header title='ReactJs Blog' width={width} />
-        <Nav
-          search={search}
-          setSearch={setSearch}
-        />
+        <Header title='ReactJs Blog' />
+        <Nav />
         <Routes>
-          <Route path="/" element={
-            <Home
-              posts={searchResults}
-              fetchError={fetchError}
-              isLoading={isLoading}
-            />} />
-          <Route path="/post" element={
-            <NewPost
-              handleSubmit={handleSubmit}
-              postTitle={postTitle}
-              setPostTitle={setPostTitle}
-              postBody={postBody}
-              setPostBody={setPostBody}
-            />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/post" element={<NewPost />} />
           {/* Edit */}
-          <Route path="/edit/:id" element={
-            <EditPost
-              posts={posts}
-              handleEdit={handleEdit}
-              editTitle={editTitle}
-              setEditTitle={setEditTitle}
-              editBody={editBody}
-              setEditBody={setEditBody}
-            />} />
+          <Route path="/edit/:id" element={<EditPost />} />
           {/* problem with post/id */}
-          <Route path="/post/:id" element={<PostPage posts={posts}
-            handleDelete={handleDelete} />} />
+          <Route path="/post/:id" element={<PostPage />} />
           <Route path="/about" element={<About />} />
           <Route path="*" element={<Missing />} />
         </Routes>
--- Front End/ReactJS/learn_reactjs_02/src/context/DataContext.js
+++ Front End/ReactJS/learn_reactjs_02/src/context/DataContext.js
@@ -1,14 +1,11 @@
 import React from "react";
 import { createContext, useState, useEffect } from "react";
 import { useHistory } from 'react-router-dom';
-import { format } from 'date-fns';
 import api from '../api/posts'
-import EditPost from '../EditPost';
-import useWindowSize from '../hooks/useWindowSize';
+import { format } from 'date-fns';
 import useAxiosFetch from '../hooks/useAxiosFetch';
 
 const DataContext = createContext({})
-
 
 export const DataProvider = ({ children }) => {
     const [posts, setPosts] = useState([])
@@ -16,16 +13,8 @@ export const DataProvider = ({ children }) => {
     // results is an array
     const [searchResults, setSearchResults] = useState([])
 
-    const [postTitle, setPostTitle] = useState('');
-    const [postBody, setPostBody] = useState('');
-
-    const [editTitle, setEditTitle] = useState('');
-    const [editBody, setEditBody] = useState('');
-
     // history
     // const history = useHistory()
-
-    const { width } = useWindowSize()
 
     const { data, fetchError, isLoading } = useAxiosFetch('http://localhost:3500/posts')
 
@@ -67,23 +56,6 @@ export const DataProvider = ({ children }) => {
         setSearchResults(filteredResults.reverse())
     }, [posts, search])
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        const id = posts.length ? posts[posts.length - 1].id + 1 : 1;
-        const datetime = format(new Date(), 'MMMM dd, yyyy pp'); // pp for time
-        const newPost = { id, title: postTitle, datetime, body: postBody }
-        try {
-            const response = await api.post('/posts', response.data)
-            const allPosts = [...posts, newPost] // an array
-            setPosts(allPosts)
-            setPostTitle('')
-            setPostBody('')
-            //history.push('/')
-        } catch (error) {
-            console.log(`Error: ${error.message}`)
-        }
-    }
-
     const handleEdit = async (id) => {
         const datetime = format(new Date(), 'MMMM dd, yyyy pp'); // pp for time
         const updatePost = { id, title: editTitle, datetime, body: editBody }
@@ -111,7 +83,9 @@ export const DataProvider = ({ children }) => {
 
     return (
         <DataContext.Provider value={{
-            width
+            search, setSearch,
+            searchResults, fetchError, isLoading,
+            posts, setPosts
         }}>
             {children}
         </DataContext.Provider>
