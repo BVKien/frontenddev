1db39838f0f1d311aed4be4458aaa9c6cce3ff8c
Bùi Văn Kiên <kbui0212@gmail.com>
2023-10-14 12:20:46 +0700

    Add: demo front end ols 01, tiktok_UI-SonDang. +Modify: ReactJs.txt

---
+++ Front End/ReactJS/demo_frontend_ols_01/src/layouts/DefaultLayout.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const DefaultLayout = () => {
+    return (
+        <main>
+            <p>----------------------------------------------------------------------</p>
+            <h1>This is index</h1>
+            <h2>Trnag này để show ra những thứ mà không cần đăng nhập vẫn có thể xem</h2>
+            <h3>Trang này sẽ để làm homepage default</h3>
+            <h1>This is Home page default</h1>
+            <p>
+                + Cần phải có:<br />
+                - Header <br />
+                - Sidebar <br />
+                - Content <br />
+                - Footer <br />
+            </p>
+        </main>
+    )
+}
+
+export default DefaultLayout
