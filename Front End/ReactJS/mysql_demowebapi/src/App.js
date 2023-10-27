4ec4ca160c2ec4462a4e19639db30622338a994b
Bùi Văn Kiên <kbui0212@gmail.com>
2023-10-27 19:58:01 +0700

    Modified: mysql demo api

--- Front End/ReactJS/mysql_demowebapi/src/App.js
+++ Front End/ReactJS/mysql_demowebapi/src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import GetAllCourses from './components/functions/GetAllCourses';
 import GetCourseById from './components/functions/GetCourseById';
+import AddCourse from './components/functions/AddCourse';
+import './index.css';
 
 function App() {
   return (
@@ -15,11 +17,32 @@ function App() {
         <li>
           <Link to='/GetCourseById'>Get Course By Id</Link>
         </li>
+        <li>
+          <Link to='/AddCourse'>Add Course</Link>
+        </li>
       </ul>
+
+      <div className='ok'  > ok </div>
+
+      <html>
+        <div class="breadcrumb-section breadcrumb-bg">
+          <div class="container">
+            <div class="row">
+              <div class="col-lg-8 offset-lg-2 text-center">
+                <div class="breadcrumb-text">
+                  <p>Chất lượng hàng đầu</p>
+                  <h1>Giỏ hàng</h1>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </html>
 
       <Routes>
         <Route path='/GetAllCourses' element={<GetAllCourses />} />
         <Route path='/GetCourseById' element={<GetCourseById />} />
+        <Route path='/AddCourse' element={<AddCourse />} />
       </Routes>
     </Router>
   );
