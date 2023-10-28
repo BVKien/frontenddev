dcfc6b1f39de2aa2fa0271ee72f0aa842af0efb0
Bùi Văn Kiên <kbui0212@gmail.com>
2023-10-28 17:07:40 +0700

    mysql demowebapi - Modified: AddCourse, Code_note

--- Front End/ReactJS/mysql_demowebapi/src/App.js
+++ Front End/ReactJS/mysql_demowebapi/src/App.js
@@ -3,12 +3,11 @@ import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import GetAllCourses from './components/functions/GetAllCourses';
 import GetCourseById from './components/functions/GetCourseById';
 import AddCourse from './components/functions/AddCourse';
-import './index.css';
 
 function App() {
   return (
     <Router>
-      <p>This is App, bây giờ tôi sẽ viết test crud cho api của tôi nhé.</p>
+      <p>Tôi viết giao diện API của tôi</p>
 
       <ul>
         <li>
@@ -18,32 +17,16 @@ function App() {
           <Link to='/GetCourseById'>Get Course By Id</Link>
         </li>
         <li>
-          <Link to='/AddCourse'>Add Course</Link>
+          <Link to='AddCourse'>Add Course</Link>
         </li>
       </ul>
 
-      <div className='ok'  > ok </div>
-
-      <html>
-        <div class="breadcrumb-section breadcrumb-bg">
-          <div class="container">
-            <div class="row">
-              <div class="col-lg-8 offset-lg-2 text-center">
-                <div class="breadcrumb-text">
-                  <p>Chất lượng hàng đầu</p>
-                  <h1>Giỏ hàng</h1>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </html>
-
       <Routes>
         <Route path='/GetAllCourses' element={<GetAllCourses />} />
-        <Route path='/GetCourseById' element={<GetCourseById />} />
+        <Route path='GetCourseById' element={<GetCourseById />} />
         <Route path='/AddCourse' element={<AddCourse />} />
       </Routes>
+
     </Router>
   );
 }
--- Front End/ReactJS/mysql_demowebapi/src/components/functions/GetCourseById.js
+++ Front End/ReactJS/mysql_demowebapi/src/components/functions/GetCourseById.js
@@ -6,8 +6,6 @@ const GetCourseById = () => {
     const [courseId, setCourseId] = useState('')
     const [course, setCourse] = useState(null)
 
-    // id ma nguoi dung nhap vao
-    // set courseid = gia tri event ma nguoi dung nhap
     const handleInputChange = (event) => {
         setCourseId(event.target.value)
     }
@@ -17,37 +15,46 @@ const GetCourseById = () => {
             const response = await axios.get(`https://localhost:7041/api/Course/${courseId}`)
             setCourse(response.data)
         } catch (error) {
-            console.error('Error fetching ', error)
+            throw new Error(error)
         }
     }
 
     const handleSubmit = (event) => {
-        event.preventDefault() // ngăn chặn việc tải lại trang khi nhấn enter 
-        fetchCourseById() // gọi hàm để thực hiện get course by id
+        event.preventDefault()
+        fetchCourseById()
     }
 
     return (
         <div>
-            <p>GetCourseById</p>
+            <ul>
+                <li>GetCourseById</li>
+            </ul>
             <form onSubmit={handleSubmit}>
-                <label htmlFor='courseIdInput'>Enter Course Id: </label>
+                <label htmlFor="courseIdInput">Enter course Id: </label>
                 <input
                     type='number'
                     id='courseIdInput'
                     value={courseId}
                     onChange={handleInputChange}
                 />
-                <button type='submit'>fetch course</button>
+                <button type='submit'>Fetch course</button>
             </form>
-            {course ? (
-                <div>
-                    <p>course Id: {course.courseId}</p>
-                </div>
-            ) : (
-                <p> Enter valid course Id and click 'fetch course' to see the detalis.</p>
-            )
-            }
-        </div >
+
+            {course ? (<div>
+                <p>Course Id: {course.courseId}</p>
+                <p>courseName: {course.courseName}</p>
+                <p>description: {course.description}</p>
+                <p>courseInfo: {course.courseInfo}</p>
+                <p>image: {course.image}</p>
+                <p>videoIntro: {course.videoIntro}</p>
+                <p>fee: {course.fee}</p>
+                <p>status: {course.status}</p>
+                <p>categoryCategoryId: {course.categoryCategoryId}</p>
+                <p>userUserId: {course.userUserId}</p>
+            </div>) : (<p>
+                Enter valid course Id and click 'fetch course' to see detail
+            </p>)}
+        </div>
     )
 }
 
