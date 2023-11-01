e95c3bf5a557747257f6aa1ddd9e0f381b295f6b
Bùi Văn Kiên <kbui0212@gmail.com>
2023-11-01 15:39:53 +0700

    Add: tiktok-ui-main-ver02, practice to build front end

--- Front End/ReactJS/mysql_demowebapi/src/App.js
+++ Front End/ReactJS/mysql_demowebapi/src/App.js
@@ -1,16 +1,28 @@
-import React from 'react';
-import './index.css'
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import GetAllCourses from './components/functions/GetAllCourses';
-import GetCourseById from './components/functions/GetCourseById';
-import AddCourse from './components/functions/AddCourse';
-import UpdateCourse from './components/functions/UpdateCourse';
-import DeleteCourse from './components/functions/DeleteCourse';
-import DeleteCourseUsingAxios from './components/functions/DeleteCourseUsingAxios';
+// libs
+import React from "react";
+import "./index.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
+// layouts
+import Header from "./components/layouts/Header";
+import Footer from "./components/layouts/Footer";
+import Nav from "./components/layouts/Nav";
+import Sidebar from "./components/layouts/Sidebar";
+import Content from "./components/layouts/Content";
+
+// functions components
+import GetAllCourses from "./components/functions/GetAllCourses";
+import GetCourseById from "./components/functions/GetCourseById";
+import AddCourse from "./components/functions/AddCourse";
+import UpdateCourse from "./components/functions/UpdateCourse";
+import DeleteCourse from "./components/functions/DeleteCourse";
+import DeleteCourseUsingAxios from "./components/functions/DeleteCourseUsingAxios";
+import Button from "./components/button/Button";
 
 function App() {
   return (
     <Router>
+      <hr />
       <ul>
         {/* css inline */}
         {/* <p style={{ color: 'red' }}>Tôi viết giao diện API của tôi</p> */}
@@ -19,34 +31,49 @@ function App() {
 
       <ul>
         <li>
-          <Link to='/GetAllCourses'>Get All Courses</Link>
+          <a href="https://react.dev/">React</a>
         </li>
         <li>
-          <Link to='/GetCourseById'>Get Course By Id</Link>
+          <Link to="/GetAllCourses">Get All Courses</Link>
         </li>
         <li>
-          <Link to='AddCourse'>Add Course</Link>
+          <Link to="/GetCourseById">Get Course By Id</Link>
         </li>
         <li>
-          <Link to='/UpdateCourse'>Update Course By Id</Link>
+          <Link to="AddCourse">Add Course</Link>
         </li>
         <li>
-          <Link to='/DeleteCourse'>Delete Course</Link>
+          <Link to="/UpdateCourse">Update Course By Id</Link>
         </li>
         <li>
-          <Link to='/DeleteCourseUsingAxios'>Delete Course - Axios</Link>
+          <Link to="/DeleteCourse">Delete Course</Link>
+        </li>
+        <li>
+          <Link to="/DeleteCourseUsingAxios">Delete Course - Axios</Link>
         </li>
       </ul>
 
-      <Routes>
-        <Route path='/GetAllCourses' element={<GetAllCourses />} />
-        <Route path='GetCourseById' element={<GetCourseById />} />
-        <Route path='/AddCourse' element={<AddCourse />} />
-        <Route path='/UpdateCourse' element={<UpdateCourse />} />
-        <Route path='/DeleteCourse' element={<DeleteCourse />} />
-        <Route path='/DeleteCourseUsingAxios' element={<DeleteCourseUsingAxios />} />
-      </Routes>
+      <Button />
+      <Button primary />
+      <Button primary disabled />
+      <hr />
 
+      <Header />
+      <Nav />
+      <Sidebar />
+      <Content />
+      <Routes>
+        <Route path="/GetAllCourses" element={<GetAllCourses />} />
+        <Route path="GetCourseById" element={<GetCourseById />} />
+        <Route path="/AddCourse" element={<AddCourse />} />
+        <Route path="/UpdateCourse" element={<UpdateCourse />} />
+        <Route path="/DeleteCourse" element={<DeleteCourse />} />
+        <Route
+          path="/DeleteCourseUsingAxios"
+          element={<DeleteCourseUsingAxios />}
+        />
+      </Routes>
+      <Footer />
     </Router>
   );
 }
